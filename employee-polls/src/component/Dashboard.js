f040f28d3e10404eeb5aa58d101c3c563ca78e5e
Ngoc Tho <tho@Thos-Mac-mini.local>
2024-05-02 20:51:38 +0700

    update dashboard :

--- employee-polls/src/component/Dashboard.js
+++ employee-polls/src/component/Dashboard.js
@@ -1,30 +1,38 @@
 import { connect } from "react-redux";
 import Question from "./Question";
+import { useState } from "react";
 
 const Dashboard = ({ questions, authedUser, users }) => {
+  const [option, setOption] = useState("1");
+
   const doneQuestionIds = users[authedUser]
     ? Object.keys(users[authedUser].answers)
     : [];
+
+  const displayedQuestions = Object.values(questions).filter((q) =>
+    option === "1"
+      ? !doneQuestionIds.includes(q.id)
+      : doneQuestionIds.includes(q.id)
+  );
   return (
     <>
       <div className="container">
-        <h2 className="pb-2 border-bottom">New Questions</h2>
+        <h2 className="pb-2 border-bottom">
+          <select
+            onChange={(e) => {
+              setOption(e.target.value);
+            }}
+            class="form-select form-select-lg mb-3"
+            aria-label=".select option"
+          >
+            <option value="1">New Questions</option>
+            <option value="2">Done</option>
+          </select>
+        </h2>
         <div className="row gap-2">
-          {Object.values(questions)
-            .filter((q) => !doneQuestionIds.includes(q.id))
-            .map((question) => (
-              <Question id={question.id} key={question.id} />
-            ))}
-        </div>
-      </div>
-      <div className="container">
-        <h2 className="pb-2 border-bottom">Done</h2>
-        <div className="row gap-2">
-          {Object.values(questions)
-            .filter((q) => doneQuestionIds.includes(q.id))
-            .map((question) => (
-              <Question id={question.id} key={question.id} />
-            ))}
+          {displayedQuestions.map((question) => (
+            <Question id={question.id} key={question.id} />
+          ))}
         </div>
       </div>
     </>
