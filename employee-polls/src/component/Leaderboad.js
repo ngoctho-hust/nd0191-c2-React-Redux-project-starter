475273cfa2a96ab42d7a2d5c6e50c8f4aec24421
Ngoc Tho <tho@Thos-Mac-mini.local>
2024-05-02 20:55:53 +0700

    update leaderboard sorting

--- employee-polls/src/component/Leaderboad.js
+++ employee-polls/src/component/Leaderboad.js
@@ -44,7 +44,10 @@ const Leaderboard = ({ users }) => {
 
 const mapStateToProps = ({ users }) => ({
   users: Object.values(users).sort(
-    (a, b) => Object.keys(b.answers).length - Object.keys(a.answers).length
+    (a, b) =>
+      Object.keys(b.answers).length +
+      b.questions.length -
+      (Object.keys(a.answers).length + a.questions.length)
   ),
 });
 
